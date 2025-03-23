765fb9ec5368785cea93b8d94a47ebc03db58d35
Николашин Вадим <vadicool02@bk.ru>
2025-03-24 02:18:29 +0300

    Deleted comments and debug info

--- EvalVisitor.cpp
+++ EvalVisitor.cpp
@@ -29,52 +29,29 @@ public:
 	}
     }
 
-    antlrcpp::Any visitTypes(myGrammarParser::TypesContext *ctx) override {
-	    //std::cout << "Was there" << std::endl;
-	   if (!(ctx->bool_var() || ctx->int_var())) {
-		   std::cout << "WHYYYY" << std::endl;;
-		   return std::string("777");
-	   }
-	   std::cout << "bool " << (bool)ctx->bool_var() << std::endl;
-	   std::cout << "int " << (bool)ctx->int_var() << std::endl;
-	   std::cout << "types " << (bool)ctx->types() << std::endl;
+antlrcpp::Any visitTypes(myGrammarParser::TypesContext *ctx) override {
+	if (!(ctx->bool_var() || ctx->int_var())) {
+		return std::any();
+	}
 
-          if (ctx->bool_var()) {
-              // Visit the bool_var rule and return the result
-              //return visit(ctx->bool_var());
-	
-	  antlrcpp::Any temp = visit(ctx->bool_var());
-          } else if (ctx->int_var()) {
-              // Visit the int_var rule and return the result
-	      //std::string ss = visit(ctx->int_var()).as<std::string>());
-	      antlrcpp::Any temp = visit(ctx->int_var());
-	  try {	
-		//if (temp.type() == typeid(int)) std::cout << "INT !!!";
-		//if (temp.type() == typeid(std::string)) std::cout << "STRING !!!";
-		//if (temp.is<std::string>()) std::cout << "STRING !!!";
-		
-		
-	        //std::cout << "Name " << visit(ctx->int_var()) << std::endl;
-	        //std::string ss = std::any_cast<std::string>(visit(ctx->int_var()));
-	    }
-	    catch(...) {
-	        // std::cout << "It is " << "lag" << std::endl;
-	    }
-        //return  std::any_cast<std::string>(temp);
+	if (ctx->bool_var()) {
+	    visit(ctx->bool_var());
+    } else if (ctx->int_var()) {
+	    visit(ctx->int_var());
     }
+	  
     if (ctx->types()) {
-	visit(ctx->types());	
+	    visit(ctx->types());	
     }
-    return std::string("666");
-
-	//  return visitChildren(ctx);
+	  
+    return std::any();
   }
 
-  antlrcpp::Any visitVar_name(myGrammarParser::Var_nameContext *ctx) override {
-	  return ctx->NAME(); // change latter
+antlrcpp::Any visitVar_name(myGrammarParser::Var_nameContext *ctx) override {
+	  return ctx->NAME();
   }
 
-  antlrcpp::Any visitBool_var(myGrammarParser::Bool_varContext *ctx) override {
+antlrcpp::Any visitBool_var(myGrammarParser::Bool_varContext *ctx) override {
 
 	 auto vecNameOld = std::any_cast<std::vector<antlr4::tree::TerminalNode *>>(visit(ctx->var_name()));
 
@@ -92,34 +69,30 @@ public:
          }
 
 	 return value;
-  }
+}
 
-  antlrcpp::Any visitInt_var(myGrammarParser::Int_varContext *ctx) override { 
-	  auto vecNameOld = std::any_cast<std::vector<antlr4::tree::TerminalNode *>>(visit(ctx->var_name()));
+antlrcpp::Any visitInt_var(myGrammarParser::Int_varContext *ctx) override { 
+    auto vecNameOld = std::any_cast<std::vector<antlr4::tree::TerminalNode *>>(visit(ctx->var_name()));
 
-	  std::vector<std::string> vecName;
-	 for (auto x : vecNameOld) {
-		vecName.push_back(x->getText());
+    std::vector<std::string> vecName;
+	for (auto x : vecNameOld) {
+        vecName.push_back(x->getText());
 		std::cout << x->getText() << std::endl;
-	 }
+	}
 	  
-	  std::string value = std::any_cast<std::string>(visit(ctx->int_expr()));
-	  std::cout << value << std::endl;
+    std::string value = std::any_cast<std::string>(visit(ctx->int_expr()));
+    std::cout << value << std::endl;
 
-	  for (auto name : vecName) {
-	  	variables[name] = value;
-	  }
+    for (auto name : vecName) {
+	    variables[name] = value;
+    }
 	
     return value;
-  }
+}
 
-  antlrcpp::Any visitInt_expr(myGrammarParser::Int_exprContext *ctx) override {
-//	  std::cout << "int_expr" << std::endl;
-	std::cout << "Value:: " << std::string(ctx->NUM()->getText()) << std::endl;
-
+antlrcpp::Any visitInt_expr(myGrammarParser::Int_exprContext *ctx) override {
 	int result = std::stoi(ctx->NUM()->getText());
 
-	try {
 	if (ctx->int_expr_tail()) {
 		antlrcpp::Any tempAny = visit(ctx->int_expr_tail());
 		std::vector<std::string> temp = std::any_cast<std::vector<std::string>>(tempAny);
@@ -130,27 +103,18 @@ public:
 
 		for (int i = 0; i < temp.size(); i+=2) {
 			if (isNumber(temp[i + 1])) {
-				std::cout << "Oper" << temp[i] << std::endl;
-				std::cout << "Val" << temp[i+1] << std::endl;
 				if (temp[i] == "+") result+=stoi(temp[i+1]);
 				else if (temp[i] == "-") result-=stoi(temp[i+1]);
 				else std::cout << "Error: Unknown operation" << std::endl;
 			}
 			else std::cout << "Error: Not a number" << std::endl;
 		}
-
-		std::cout << std::endl << "Result: " << result << std::endl;
-	
 	}
-	}
-	catch(...) {
-		std::cout << "ERROR 2" << std::endl;
-	}
-        return std::to_string(result);
+    return std::to_string(result);
   }
 
-  antlrcpp::Any visitInt_expr_tail(myGrammarParser::Int_expr_tailContext *ctx) override {
-  	if (!ctx->int_oper()) return std::vector<std::string>();
+antlrcpp::Any visitInt_expr_tail(myGrammarParser::Int_expr_tailContext *ctx) override {
+    if (!ctx->int_oper()) return std::vector<std::string>();
 	  
 	std::vector<std::string> res;
 	res.push_back((std::string)ctx->int_oper()->getText());
@@ -160,62 +124,53 @@ public:
 
 	res.insert(res.end(), tempVec.begin(), tempVec.end());
 
-	for (auto x : res) {
-          std::cout << "expr_tail" << x << std::endl;
-        }
-	  
 	return res;
   }
 
-  antlrcpp::Any visitInt_oper(myGrammarParser::Int_operContext *ctx) override {
+antlrcpp::Any visitInt_oper(myGrammarParser::Int_operContext *ctx) override {
 
     return visitChildren(ctx);
   }
 
-  antlrcpp::Any visitBool_expr(myGrammarParser::Bool_exprContext *ctx) override {
-	  bool result = (std::string(ctx->bool_val()->getText()).find("TRUE") != std::string::npos);
+antlrcpp::Any visitBool_expr(myGrammarParser::Bool_exprContext *ctx) override {
+	bool result = (std::string(ctx->bool_val()->getText()).find("TRUE") != std::string::npos);
 
-	  antlrcpp::Any tempAny = visit(ctx->bool_expr_tail());
-	  // contains array || bool_val
-	  std::vector<std::string> temp = std::any_cast<std::vector<std::string>>(tempAny);
-	
-	  for (int i = 0; i < temp.size(); i+=2) {
-          	if (temp[i] == "||") {
+	antlrcpp::Any tempAny = visit(ctx->bool_expr_tail());
+	// contains array || bool_val
+	std::vector<std::string> temp = std::any_cast<std::vector<std::string>>(tempAny);
+
+	for (int i = 0; i < temp.size(); i+=2) {
+		if (temp[i] == "||") {
 			if (temp[i+1] == "TRUE") result = result || true;
 			else if (temp[i+1] == "FALSE") result = result || false;
 			else std::cout << "Error: Unknown type" << std::endl;
 		}
-                else std::cout << "Error: Unknown operation" << std::endl;
-	  }
-	  std::cout << "Result: " << result << std::endl;
-    	  return result ? std::string("true") : std::string("false");
-  }
+		else std::cout << "Error: Unknown operation" << std::endl;
+    }
+	  
+	return result ? std::string("true") : std::string("false");
+}
 
-  antlrcpp::Any visitBool_expr_tail(myGrammarParser::Bool_expr_tailContext *ctx) override {
-	  std::cout << "WAS THERE BOOL";
-       if (!ctx->bool_oper()) return std::vector<std::string>();
-	std::cout << "WAS THERE BOOL2";
-        std::vector<std::string> res;
-        res.push_back((std::string)ctx->bool_oper()->getText());
-        res.push_back((std::string)ctx->bool_val()->getText());
+antlrcpp::Any visitBool_expr_tail(myGrammarParser::Bool_expr_tailContext *ctx) override {
+	if (!ctx->bool_oper()) return std::vector<std::string>();
+	
+	std::vector<std::string> res;
+	res.push_back((std::string)ctx->bool_oper()->getText());
+	res.push_back((std::string)ctx->bool_val()->getText());
 
-        std::vector<std::string> tempVec = std::any_cast<std::vector<std::string>>(visit(ctx->bool_expr_tail()));
-        res.insert(res.end(), tempVec.begin(), tempVec.end());
+	std::vector<std::string> tempVec = std::any_cast<std::vector<std::string>>(visit(ctx->bool_expr_tail()));
+	res.insert(res.end(), tempVec.begin(), tempVec.end());
+	
+	return res;
+}
 
-        for (auto x : res) {
-          std::cout << "expr_tail_bool " << x << std::endl;
-        }
-        return res;
-  }
-
-  antlrcpp::Any visitBool_val(myGrammarParser::Bool_valContext *ctx) override {
+antlrcpp::Any visitBool_val(myGrammarParser::Bool_valContext *ctx) override {
     return visitChildren(ctx);
-  }
+}
 
-  antlrcpp::Any visitBool_oper(myGrammarParser::Bool_operContext *ctx) override {
+antlrcpp::Any visitBool_oper(myGrammarParser::Bool_operContext *ctx) override {
     return visitChildren(ctx);
-  }
-
+}
 
 };
 
--- main.cpp
+++ main.cpp
@@ -14,8 +14,6 @@ int main(int argc, const char *argv[]) {
     std::cout << "Введите выражение: ";
     std::getline(std::cin, input);
 
-    //std::cout << std::string("int").compare(input);
-
     ANTLRInputStream inputStream(input);
     myGrammarLexer lexer(&inputStream);
     CommonTokenStream tokens(&lexer);
@@ -31,8 +29,6 @@ int main(int argc, const char *argv[]) {
                   << std::endl;
     }
 
-
-
     // Create and use the visitor
     EvalVisitor visitor;
     visitor.visit(tree);
@@ -41,47 +37,3 @@ int main(int argc, const char *argv[]) {
 
     return 0;
 }
-
-/*
-int main(int argc, const char *argv[])
-{
-    std::string input;
-    std::cout << "Введите выражение: ";
-    std::getline(std::cin, input);
-
-    ANTLRInputStream inputStream(input);
-    myGrammarLexer lexer(&inputStream);
-    CommonTokenStream tokens(&lexer);
-    myGrammarParser parser(&tokens);
-
-    for (antlr4::Token* token : tokens.getTokens()) {
-        std::cout << "Token: " << token->getText() 
-                  << ", Type: " << token->getType() 
-                  << std::endl;
-    }
-
-//tree::ParseTree *tree = parser.typeList();
-    tree::ParseTree *tree = parser.types();
-    EvalVisitor visitor;
-
-   // visitor.setVariable("c", 5);
-   // visitor.setVariable("b", 10);
-
-    try
-    {
-        int result = std::any_cast<int>(visitor.visit(tree));
-        std::cout << "Результат: " << result << std::endl;
-    }
-    catch (const std::bad_any_cast &e)
-    {
-        std::cerr << "Ошибка: некорректный тип выражения." << std::endl;
-    }
-    catch (const std::exception &e)
-    {
-        std::cerr << "Ошибка: " << e.what() << std::endl;
-    }
-
-    return 0;
-}
-
-*/
